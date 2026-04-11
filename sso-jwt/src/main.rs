4ab0c35179efd20a17d0f8bf5965177f1af24dc1
Jay Gowdy <jay@gowdy.me>
2026-04-11 14:59:24 -0700

    Allow print/eprintln in Windows-only wsl_install module

--- sso-jwt/src/main.rs
+++ sso-jwt/src/main.rs
@@ -4,6 +4,12 @@ mod cli;
 mod exec;
 mod shell_init;
 #[cfg(target_os = "windows")]
+#[allow(
+    clippy::print_stdout,
+    clippy::print_stderr,
+    clippy::useless_format,
+    let_underscore_drop
+)]
 mod wsl_install;
 
 #[allow(clippy::print_stderr, clippy::exit)]
